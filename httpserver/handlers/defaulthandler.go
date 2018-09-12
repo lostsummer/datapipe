ef004bb0e5bc6eec5c6d3446d404365d62885ba5
王昱鑫 <wangyuxin0623@emoney.cn>
2018-09-12 08:59:40 +0800

    程序输出版本信息完善

--- httpserver/handlers/defaulthandler.go
+++ httpserver/handlers/defaulthandler.go
@@ -15,9 +15,11 @@ func Index(ctx dotweb.Context) error {
 }
 
 func Test(ctx dotweb.Context) error {
-	retstr := fmt.Sprintf("user_agent: %s\nglobal_id: %s\nfirst_visit_time: %s\nclient_ip: %s\nwrite_time: %s",
+	versionInfo := fmt.Sprintf("Version: %s, Branch: %s, Build: %s, Build time: %s\n",
+		global.Version, global.Branch, global.CommitID, global.BuildTime)
+	clientInfo := fmt.Sprintf("user_agent: %s\nglobal_id: %s\nfirst_visit_time: %s\nclient_ip: %s\nnow_time: %s",
 		getUserAgent(ctx), getGlobalID(ctx), getFirstVistTime(ctx), getClientIP(ctx), getNowFormatTime())
 
-	ctx.WriteString(retstr)
+	ctx.WriteString(versionInfo + clientInfo)
 	return nil
 }
